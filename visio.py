9de5c56ce3ae40083b777628fd99795d043e89c6
Alexander Kuleshov <saha262004@gmail.com>
2024-07-13 08:54:38 +0300

    увеличил скорость работы с атакой

--- visio.py
+++ visio.py
@@ -20,29 +20,15 @@ def visual():
 
     screen = pygame.display.set_mode([1000, 1000])
 
-    while True:
+    running = True
+
+    while running:
         data = units()
 
-        myFile = open(f'{time.time()}.txt', 'w')
+        #myFile = open(f'{time.time()}.txt', 'w')
+        #myFile.write(json.dumps(data))
+        #myFile.close()
 
-        myFile.write(json.dumps(data))
-        myFile.close()
-
-        #build = get_build(data)
-        #attack = get_attack(data)
-        #move_base = get_move_base(data)
-
-        #r = command(
-        #    {
-        #        'attack': attack,
-        #        'build': build,
-        #        'moveBase': move_base
-        #    }
-        #)
-
-        #pprint(r)
-
-        # Fill the background with white
         screen.fill((255, 255, 255))
 
         base = get_base(data)
@@ -77,21 +63,16 @@ def visual():
 
         if (base is not None):
             for tower in base:
-                # Draw a solid blue circle in the center
                 pygame.draw.circle(screen, (0, 0, 255), rescale(tower.get('x'), tower.get('y')), 6)
 
         if (zombies is not None):
             for zombie in zombies:
-                # Draw a solid blue circle in the center
                 pygame.draw.circle(screen, (0, 255, 0), rescale(zombie.get('x'), zombie.get('y')), 6)
-                #pygame.draw.circle(screen, (0, 255, 0), (x_d + zombie.get('x') * 2, y_d + zombie.get('y') * 2), 2)
 
 
         if (enemy_blocks is not None):
             for enemy_block in enemy_blocks:
-                # Draw a solid blue circle in the center
                 pygame.draw.circle(screen, (255, 0, 0), rescale(enemy_block.get('x'), enemy_block.get('y')), 6)
-                #pygame.draw.circle(screen, (255, 0, 0), (x_d + enemy_block.get('x') * 2,y_d + enemy_block.get('y') * 2), 2)
 
         # Flip the display
         pygame.display.flip()
@@ -99,5 +80,5 @@ def visual():
         time.sleep(1)
 
 
-participate()
+#participate()
 visual()
