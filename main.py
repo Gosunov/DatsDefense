d11706a0c615c2c510c992e784eec2a12e034858
Alexey Gosunov <agosunov@gmail.com>
2024-07-14 09:59:46 +0300

    Returned original `main.py`

--- main.py
+++ main.py
@@ -21,47 +21,38 @@ def print_status(data: UnitResponse,
         (points, base_size, zombie_kills, gold, attacks, builds, rejected)
     )
 
-data = None
-world = None
+API = MockApi()
+# API = testServerApi
+# API = mainServerApi
 
+try:
+    starts_in_sec = API.participate().starts_in_sec
+    print('Round is starting in %ds, waiting...' % starts_in_sec)
+    sleep(starts_in_sec + 0.5)
+except:
+    pass
 
-
-def main():
-    global data, world
-    API = MockApi()
-    #API = testServerApi
-    # API = mainServerApi
-
-    try:
-        starts_in_sec = API.participate().starts_in_sec
-        print('Round is starting in %ds, waiting...' % starts_in_sec)
-        sleep(starts_in_sec + 0.75)
-    except:
-        pass
-
+while True:
+    data = API.units()
     world = API.world()
-    while True:
-        data = API.units()
-        t1 = int(time() * 10**3)
-        tleft = data.turn_ends_in_ms
-        print("Turn ends in %dms" % tleft)
 
-        if (data.turn % 10 == 0):
-            world = API.world()
+    t1 = int(time() * 10**3)
+    tleft = data.turn_ends_in_ms
+    print("Turn ends in %dms" % tleft)
 
-        cmd = get_command(data, world)
+    cmd = get_command(data, world)
 
-        resp = API.command(cmd)
-        for error in resp.errors:
-            if error == "you are dead":
-                print("We are dead :(")
-                exit(0)
-            print(error)
+    resp = API.command(cmd)
+    for error in resp.errors:
+        if error == "you are dead":
+            print("We are dead :(")
+            exit(0)
+        print(error)
 
-        print_status(data, world, cmd, resp)
-        t2 = int(time() * 10**3)
-        tused = t2 - t1
-        print("Finished turn %d in (%dms/%dms)" % (data.turn, tused, tleft))
-        textra = tleft - tused
-        if textra >= 0:
-            sleep(textra / 10**3)
+    print_status(data, world, cmd, resp)
+    t2 = int(time() * 10**3)
+    tused = t2 - t1
+    print("Finished turn %d in (%dms/%dms)" % (data.turn, tused, tleft))
+    textra = tleft - tused
+    if textra >= 0:
+        sleep(textra / 10**3)
